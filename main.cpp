02f64980bdecc1cbbff63ffef4f16679ec8551a5
Jonathan Lam <jlam323@uwo.ca>
2015-10-24 17:31:57 -0400

    Revert "Added main header, small changes to main."
    This reverts commit 63f6e8e755c436f8a9f0a729b87b00b8f5ffb108.

--- main.cpp
+++ main.cpp
@@ -1,23 +1,11 @@
 #include "mainwindow.h"
 #include <QApplication>
-#include "Parser.h"
-#include "main.h"
 
 int main(int argc, char *argv[])
 {
-	string file;
-
-
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
-
-	//TO DO, GUI gets file name input and sends back
-	//file =
-
-
-
-
 
     return a.exec();
 }
