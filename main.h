63f6e8e755c436f8a9f0a729b87b00b8f5ffb108
Jonathan Lam <jlam323@uwo.ca>
2015-10-24 17:25:19 -0400

    Added main header, small changes to main.

---
+++ main.h
@@ -0,0 +1,15 @@
+/**
+* Header file for the Parser class and its associated methods
+*/
+#ifndef MAIN_H_INCLUDED
+#define MAIN_H_INCLUDED
+
+
+#include "mainwindow.h"
+#include <QApplication>
+#include "Parser.h"
+#include "main.h"
+
+int main(int argc, char *argv[]);
+
+#endif //MAIN_H_INCLUDED
