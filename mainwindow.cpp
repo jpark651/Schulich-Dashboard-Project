4a88f874c48cc804e775bafc412c0fe67dbfb749
Logan Kember <logankember@Logans-MacBook-Pro.local>
2015-10-25 00:34:14 -0400

    Added Publication items

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,8 +1,9 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include "Parser.h"
 #include <QMessageBox>
 #include <iostream>
-#include
+#include <QStandardItemModel>
 
 using namespace std;
 
@@ -11,6 +12,39 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    ui->menuBar->setNativeMenuBar(false);
+
+
+    //Adding expandable items for Publications
+    QTreeWidgetItem *publications = new QTreeWidgetItem();
+    QTreeWidgetItem *publishedAbstracts = new QTreeWidgetItem();
+    QTreeWidgetItem *journalArticles = new QTreeWidgetItem();
+    QTreeWidgetItem *books = new QTreeWidgetItem();
+    QTreeWidgetItem *bookChapters = new QTreeWidgetItem();
+    QTreeWidgetItem *letters = new QTreeWidgetItem();
+    ui->publicationTree->addTopLevelItem(publications);
+    publications->setText(0, tr("Publications"));
+    publishedAbstracts->setText(0, tr("Published Abstracts"));
+    journalArticles->setText(0, tr("Journal Articles"));
+    books->setText(0, tr("Books"));
+    bookChapters->setText(0,tr("Book Chapters"));
+    letters->setText(0, tr("Letters to Editors"));
+
+
+    //TO DO: Get totals
+
+    publications->addChild(publishedAbstracts);
+    publications->addChild(journalArticles);
+    publications->addChild(books);
+    publications->addChild(letters);
+
+
+    //Adding expandable items for Grants and Clinical Funding Summary
+    QTreeWidgetItem *fundingSummary = new QTreeWidgetItem();
+    QTreeWidgetItem *peerReviewed = new QTreeWidgetItem();
+    peerReviewed->setText(0,tr("Peer Reviewed"));
+    fundingSummary->setText(0,tr("Grants and Clinical Funding"));
+
 }
 
 MainWindow::~MainWindow()
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,8 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QTreeView>
+#include <QTreeWidget>
 
 namespace Ui {
 class MainWindow;
@@ -10,6 +12,8 @@ class MainWindow;
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
+
+
 
 public:
     explicit MainWindow(QWidget *parent = 0);
