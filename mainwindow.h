3a65cb8448771967e879680a83e1c44c90bd076a
Logan Kember <logankember@Logans-MacBook-Pro.local>
2015-10-25 02:59:45 -0400

    Allowing the window to be resized.

--- mainwindow.h
+++ mainwindow.h
@@ -18,7 +18,7 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
-    bool activeFile = false;    //value which tells if a file has been loaded or not
+    bool activeFile;    //value which tells if a file has been loaded or not
 
 private:
     Ui::MainWindow *ui;
